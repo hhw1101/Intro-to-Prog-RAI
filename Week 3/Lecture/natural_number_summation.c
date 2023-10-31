f3459b3517ccdaa1133bc4de0f717a51472d333a
hhw1101 <jenniferhuang2002@gmail.com>
2023-10-31 11:14:50 +0000

    Week 3 finish summation

--- Week 3/Lecture/natural_number_summation.c
+++ Week 3/Lecture/natural_number_summation.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
-int main(){
+int findSummation(int num);
+
+int main()
+{
     int number;
     puts("Enter a number: ");
     scanf("%d", &number);
-    
+
     int loopSummation = 0;
     // In C the curly bracket is not necessary.
     for (int i = 0; i <= number; i++)
         loopSummation += i;
-    
+
     printf("The summation of all the natrual numbers up to %d is %d.", number, loopSummation);
+    printf("\nThe summation of all the natrual numbers up to %d is %d.", number, findSummation(number));
+
 }
 
-int findSummation(int num){
-
+int findSummation(int num)
+{
+    if (num == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return num + findSummation(num - 1);
+    }
 }
