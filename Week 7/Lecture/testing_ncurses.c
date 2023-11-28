9d9f809edce004361573de5a8ae9fc0941b500ce
hhw1101 <jenniferhuang2002@gmail.com>
2023-11-28 16:27:51 +0000

    Fixing ncurses testing complete, ncurses works on windows

--- Week 7/Lecture/testing_ncurses.c
+++ Week 7/Lecture/testing_ncurses.c
@@ -1,6 +1,29 @@
-#include<stdio.h>
-#include <ncurses\ncurses.h>
+#include <ncurses/ncurses.h>
 
-int main(){
-    
+int main()
+{
+    int ch;
+    initscr();
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+    printw("Type any character to see it in bold\n");
+    while (1)
+    {
+        ch = getch();
+        if (ch == KEY_LEFT)
+            printw("Left arrow is pressed\n");
+        else if (ch == KEY_RIGHT)
+            printw("Right arrow is pressed\n");
+        else if (ch == KEY_UP)
+            printw("Up arrow is pressed\n");
+        else if (ch == KEY_DOWN)
+            printw("Down arrow is pressed\n");
+        // if ESC is pressed, end the program
+        else if (ch == 27)
+            break;
+    }
+    // getch();
+    endwin();
+    return 0;
 }
